a7b8944670d00136fa89451ceb949bb58e3035e4
Jan Vincent Liwanag <jvliwanag@gmail.com>
2025-11-03 11:43:47 +0800

    Init with uniwind

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./global.css";
+
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="bg-foo flex-1 items-center justify-center">
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
